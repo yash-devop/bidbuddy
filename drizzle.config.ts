9cd0866b7d813abd69b70a8d3cdda7b9138bae9c
yash-devop <kamble1234meena@gmail.com>
2024-05-30 11:01:12 +0530

    setting up postgres + drizzle

---
+++ drizzle.config.ts
@@ -0,0 +1,12 @@
+import { env } from '@/env'
+import {defineConfig} from 'drizzle-kit'
+export default defineConfig({
+    schema: "./src/db/schema.ts",
+    dialect:"postgresql",
+    out:"./drizzle",
+    dbCredentials:{
+        url: env.DATABASE_URL
+    },
+    verbose: true,
+    strict: true
+})
---
+++ src/db/schema.ts
@@ -0,0 +1,7 @@
+import { pgTable, serial } from "drizzle-orm/pg-core";
+
+
+
+export const bids = pgTable("bb_bids",{
+    id:  serial("id").primaryKey()
+})
